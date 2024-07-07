6dd21563613f9fa36be54033f6c57c568f5ea62f
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-07-07 21:53:19 +0500

    code update

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
 // 6. Smallest Number
-// var arr = [];
-// for (var i = 0; i < 5; i++) {
-//     var userInput = +prompt("Enter a number : ");
-//     arr.push(userInput);
-// }
-
-
-
-// var smallestNbr = arr[0];
-// for (var j = 0; j < arr.length; j++) {
-//   if (arr[j] < smallestNbr) {
-//     smallestNbr = arr[j];
-//   }
-// }
-// console.log(`Smallest number is : ${smallestNbr}`);
+var arr = [];
+for (var i = 0; i < 5; i++) {
+    var userInput = +prompt("Enter a number : ");
+    arr.push(userInput);
+}
+var smallestNbr = arr[0];
+for (var j = 0; j < arr.length; j++) {
+  if (arr[j] < smallestNbr) {
+    smallestNbr = arr[j];
+  }
+}
+console.log(`Smallest number is : ${smallestNbr}`);
 
 
 
 // 7. Celcius to Fahrenheit
-// var celciusInput = +prompt("Enter an number : ")
-// var FahrenheitOutput = celciusInput * (9/5) + 32
-// console.log(FahrenheitOutput);
+var celciusInput = +prompt("Enter an number : ")
+var FahrenheitOutput = celciusInput * (9/5) + 32
+console.log(FahrenheitOutput);
 
 
 
 // 8. Fahrenheit to Celcius
-// var fahrenhietInput = +prompt("Enter an number : ");
-// var celciusOutput = ((fahrenhietInput - 32) * 5) / 9;
-// console.log(celciusOutput);
+var fahrenhietInput = +prompt("Enter an number : ");
+var celciusOutput = ((fahrenhietInput - 32) * 5) / 9;
+console.log(celciusOutput);
 
 
 
 // 9. reverse an array
-// var array = [];
-// var reverse = [];
-// for (var i = 0; i < 5; i++) {
-//   var userArr = prompt("Enter something : ");
-//   array.push(userArr);
-// }
-// console.log(array);
-
-// for (var j = array.length - 1; j >= 0; j--) {
-//   reverse.push(array[j]);
-// }
-// console.log(reverse);
+var array = [];
+var reverse = [];
+for (var i = 0; i < 5; i++) {
+  var userArr = prompt("Enter something : ");
+  array.push(userArr);
+}
+console.log(array);
+for (var j = array.length - 1; j >= 0; j--) {
+  reverse.push(array[j]);
+}
+console.log(`Reversed array is : ${reverse}`);
 
 
 
 // 10. Average of an array
-// var arr = [];
-// var sum = 0;
-// for (var i = 0; i < 5; i++) {
-//   var userInput = +prompt("Enter a number : ");
-//   arr.push(userInput);
-// }
-// console.log(arr);
-// for (var j = 0; j < arr.length; j++) {
-//   sum += arr[j];
-// }
-// var average = sum / arr.length;
-// console.log(`Average is ${average}`);
+var arr = [];
+var sum = 0;
+for (var i = 0; i < 5; i++) {
+  var userInput = +prompt("Enter a number : ");
+  arr.push(userInput);
+}
+console.log(arr);
+for (var j = 0; j < arr.length; j++) {
+  sum += arr[j];
+}
+var average = sum / arr.length;
+console.log(`Average is ${average}`);
